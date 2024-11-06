c67b448aaf697be516a3627857f63fd8261d3e7e
unknown <debalina.bandyopadhyay@gmail.com>
2024-11-06 12:58:34 +0530

    First commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:15,
+    autoplay:true,
+    nav:false,
+    dots:false,
+    responsive:{
+        0:{
+            items:3
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:4
+        }
+    }
+})
+});
